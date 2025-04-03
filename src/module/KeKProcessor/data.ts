b21679d3c8f835ed3deea43dfb684b7f8155386e
Kamaz91 <Kamaz91@gmail.com>
2025-04-04 00:39:44 +0200

    added sandbox channel

--- src/module/KeKProcessor/data.ts
+++ src/module/KeKProcessor/data.ts
@@ -1,7 +1,7 @@
 export var CursedUsersId = ["166919079305805824"];
 export var BlessedUsersId = ["166912939079499776", "166956114154356736"];
 
-export var ALLOWED_CHANNELS = ["539972259771711519", "1356308552893071521"];
+export var ALLOWED_CHANNELS = ["539972259771711519", "1356308552893071521", "647947021646430208"];
 
 export const ALLOWED_CONTENT_TYPES = ["image/png", "image/jpeg", "image/jpg", "image/webp", "image/bmp", "image/tiff"];
 
